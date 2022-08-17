898d38ce66e1f457380ff1c01ad099e11e86b69a
LasttoSay <lasttosaytoyou@gmail.com>
2022-08-17 22:40:15 +0200

    Added dropdown to catalog. Created slider section

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,18 @@
 $(function(){
+  
+  
+  const catalog = document.querySelector('.catalog');
+  const dropdown = document.querySelector('.catalog');
+  dropdown.addEventListener('click', () => {
+    catalog.classList.toggle('active');
+  });
 
+  $('.slider__content').slick({
+    arrows: false,
+    dots: true,
+    autoplay: true,
+    autoplaySpeed: 3000,
+    speed: 1500,
+    fade: true,
+ });
 });
